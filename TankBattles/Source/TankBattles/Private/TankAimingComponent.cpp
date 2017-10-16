46f387f86613e265d1dce094951d2120cb81746b
DAN-PC\Dan <danriley1505@hotmail.com>
2017-10-16 14:06:25 +0100

    BluePrintCallable()

--- TankBattles/Source/TankBattles/Private/TankAimingComponent.cpp
+++ TankBattles/Source/TankBattles/Private/TankAimingComponent.cpp
@@ -15,6 +15,10 @@ UTankAimingComponent::UTankAimingComponent()
 	// ...
 }
 
+void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	Barrel = BarrelToSet;
+}
 
 // Called when the game starts
 void UTankAimingComponent::BeginPlay()
@@ -37,5 +41,6 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 void UTankAimingComponent::AimAt(FVector OutHitLocation)
 {
 	auto OurTankName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *OurTankName, *OutHitLocation.ToString());
+	auto BarrelLocation = Barrel->GetComponentLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *OutHitLocation.ToString(), *BarrelLocation);
 }
--- TankBattles/Source/TankBattles/Public/TankAimingComponent.h
+++ TankBattles/Source/TankBattles/Public/TankAimingComponent.h
@@ -16,14 +16,20 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-protected:
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
+	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	void AimAt(FVector OutHitLocation);
 	
+private:
+	UStaticMeshComponent* Barrel = nullptr;
+
+
 };
+
+
