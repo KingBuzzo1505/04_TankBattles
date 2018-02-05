a8095fa585025aa84c8870493fe685d767cd1043
DAN-PC\Dan <danriley1505@hotmail.com>
2018-02-05 12:38:01 +0000

    Challenge: Turret Rotation Pt. 2

--- TankBattles/Source/TankBattles/Private/TankTurret.cpp
+++ TankBattles/Source/TankBattles/Private/TankTurret.cpp
@@ -1,7 +1,16 @@
 // Daniel Riley 2017
 
 #include "TankTurret.h"
+#include "TankBattles.h"
 
+
+void UTankTurret::Rotate(float RelativeSpeed)
+{
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange;
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+}
 
 
 
